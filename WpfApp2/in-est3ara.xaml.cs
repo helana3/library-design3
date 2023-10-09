68fd583a5efa2da3f984dccd24534f0fa89cd8fd
helana3 <131589618+helana3@users.noreply.github.com>
2023-10-09 20:40:25 +0300

    Add files via upload

---
+++ WpfApp2/in-est3ara.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for in_est3ara.xaml
+    /// </summary>
+    public partial class in_est3ara : Page
+    {
+        public in_est3ara()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
